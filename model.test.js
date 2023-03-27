a135564ae5bb26d6586a6f24222e7f998e8513b6
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-27 17:27:02 +0100

    Test made for model class to see if obj is made

--- model.test.js
+++ model.test.js
@@ -1,5 +1,12 @@
 notes = require("./model")
 describe('testing model class that has notes',() =>
 {
+  it('tests to see if model object can be made and just fine',() =>
+  {
+    newNotes = new notes()
+    expect(newNotes.notes).toEqual([])
+  });
     
+
+  
 });

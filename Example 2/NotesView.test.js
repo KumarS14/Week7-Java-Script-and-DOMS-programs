3447f14d3467c08cc9eb1548acbffaa6aa042047
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-29 17:29:49 +0100

    test to see if class can be called

--- Example 2/NotesView.test.js
+++ Example 2/NotesView.test.js
@@ -1,1 +1,17 @@
+/**
+ * @jest-environment jsdom
+ */
 
+const { equal } = require('assert');
+const fs = require('fs');
+ ViewNotes = require('./NotesView');
+
+describe('Page View', () => {
+    it('displays zero notes', () => {
+      
+  
+      const viewNotes = new ViewNotes();
+      expect(viewNotes.notes).toEqual([])
+      
+    });
+  });

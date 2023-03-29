6d23fc2d7bf24936679780258114f86c41af2caf
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-29 17:44:05 +0100

    test to see if two classes are working together

--- Example 2/view.test.js
+++ Example 2/view.test.js
@@ -5,6 +5,7 @@
 const { equal } = require('assert');
 const fs = require('fs');
 const View = require('./view');
+const ViewNotes = require('./view')
 
 describe('Page view', () => {
   it('displays 2 paragraphs', () => {
@@ -29,6 +30,16 @@ describe('Page view', () => {
     view.clearParagraph()
 
     expect(document.querySelectorAll('p').length).toBe(0);
+  });
+  it('should retrive notes from notes class and call them in the view class using getNotes',() => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const notes = new ViewNotes();
+    notes.addNote('Finish by 6')
+    const view = new View()
+    view.getNotes()
+    expect(document.querySelectorAll('div').length).toBe(1);
+
+
   });
   
  

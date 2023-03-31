f111805d5ad13701d8ecdb99c8957eef0856b2ad
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-31 11:17:12 +0100

    added test to see if message is returned

--- Example 3/example-3/messageView.test.js
+++ Example 3/example-3/messageView.test.js
@@ -16,4 +16,17 @@ describe('MessageView', () => {
 
     expect(document.querySelector('#message')).not.toBeNull();
   });
+  it('clicks the button and returns message', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+
+    const view = new MessageView();
+
+    const buttonEl = document.querySelector('#show-message-button');
+    const barEl = document.querySelector('#message-input');
+    barEl.value = "Sonjay"
+    buttonEl.click();
+
+    expect(document.querySelector('#message').innerText).toBe("sonjay")
+  });
+  
 });

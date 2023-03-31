67274e0697cf0d6d3d4c1e320a4f072a6a239473
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-31 11:38:18 +0100

    removed old code

--- Example 1/index.js
+++ Example 1/index.js
@@ -1,7 +1,3 @@
 model = require("./model")
 new_model = new model()
-new_model.addNote("Swimming")
-new_model.addNote("Shopping")
-new_model.addNote("Surfing")
-console.log(new_model.getNotes())
-console.log("the notes app is running");
+new_model.addNote()

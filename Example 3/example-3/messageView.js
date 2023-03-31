6414dcf5ded52760ea93177eca43ce752597ff59
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-31 09:42:43 +0100

    added method that hides message

--- Example 3/example-3/messageView.js
+++ Example 3/example-3/messageView.js
@@ -1,14 +1,24 @@
 class MessageView {
   constructor() {
     this.buttonEl = document.querySelector('#show-message-button');
-
+    this.buttonEle = document.querySelector('#hide-message-button')
     this.buttonEl.addEventListener('click', () => {
        this.displayMessage();
     });
+    this.buttonEle.addEventListener('click',() => {
+      this.hideMessage();
+    });
   }
-
+  hideMessage(){
+    const main = document.querySelector('#message')
+    main.remove()
+  }
   displayMessage() {
-    console.log('Thanks for clicking me!');
+   const message = document.createElement('div')
+   message.innerText = "This is a button";
+   message.id = 'message';
+   document.querySelector('#main-container').append(message)
+
   }
 }
 

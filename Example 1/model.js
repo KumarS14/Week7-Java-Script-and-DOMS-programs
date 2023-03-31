ed53b4049d20e393c0f434e62cef20d6863b841d
KumarS14 <sonjaykumar1404@gmail.com>
2023-03-31 11:56:53 +0100

    query for button html and added event listener

--- Example 1/model.js
+++ Example 1/model.js
@@ -2,11 +2,14 @@ class Model {
     constructor()
     {
         this.containerEl = document.querySelector('#main-container')
-        
+        this.buttonEl = document.querySelector('#add-note-button')
+        this.buttonEl.addEventListener('click',() => {
+            this.addNote()
+        });
     }
-    addNote(note){
+    addNote(){
         const newNote = document.createElement('p');
-        newNote.textContent = note;
+        newNote.textContent = 'note';
         newNote.id = 'note' ;
         this.containerEl.append(newNote);
     }
